5b8d4a9101e55a5279a9d7cbe6e20d5e8648fd6a
Henrique p. Bernardes <henriquepabe@gmail.com>
2021-12-03 01:25:21 -0300

    first commit

---
+++ dicionario/src/App.js
@@ -0,0 +1,13 @@
+import Input from "./components/Inputs";
+import ImgGenerator from "./components/ImgGenerator";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ dicionario/src/components/ImgGenerator.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Style from "./styles/dicionario.module.css"
+import Inputs from "./Inputs"
+
+export default function ImageGenerator(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
